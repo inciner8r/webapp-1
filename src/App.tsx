64e50c053787787ee0a378d15cbb63216e16a6c8
adimis-ai <adimis.ai.001@gmail.com>
2023-04-07 23:17:02 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import MyReviews from './pages/MyReviews';
 import AllReviews from './pages/AllReviews';
