af5715c5898b332dd198eac199d9b1429e41b76c
Devsi <grace.emily876@gmail.com>
2023-11-18 12:37:01 +0530

    update profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -9,7 +9,8 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
 import Cookies from "js-cookie";
-import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import React, { useEffect, useState, ChangeEvent, FormEvent} from "react";
 
 export interface FlowIdResponse {
   eula: string;
@@ -20,9 +21,36 @@ export interface WalletData {
   walletAddress: string | undefined;
 }
 
+interface FormData {
+  name: string;
+  discord: string;
+  twitter: string;
+  country: string;
+  profilePicture: File | null;
+}
+
 const Profile = () => {
 
   const [loading, setLoading] = useState<boolean>(false);
+  const [profileset, setprofileset] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>("");
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timeoutId = setTimeout(() => {
+      const storedSubmissionProfile = localStorage.getItem('submissionProfile');
+      if (storedSubmissionProfile === 'true') {
+        setprofileset(true);
+      }
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => {
+      clearTimeout(timeoutId);
+      setLoading(false);
+    }
+  }, [msg]);
 
   const bg = {
     backgroundColor: "#222944",
@@ -37,6 +65,75 @@ const Profile = () => {
     backgroundColor: "#11D9C5",
   };
 
+  const initialFormData: FormData = {
+    name: '',
+    country: '',
+    profilePicture: null,
+    discord: '',
+    twitter: '',
+  };
+
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const auth = Cookies.get("platform_token");
+
+    try {
+      const formDataObj = new FormData();
+      formDataObj.append('name', formData.name);
+      formDataObj.append('country', formData.country);
+      formDataObj.append('discord', formData.discord);
+      formDataObj.append('twitter', formData.twitter);
+      if (formData.profilePicture) {
+        formDataObj.append('profilePicture', formData.profilePicture);
+      }
+
+      // Convert FormData to JavaScript Object
+const formDataObject: { [key: string]: string | File | null } = {};
+formDataObj.forEach((value, key) => {
+  formDataObject[key] = value;
+});
+
+// Convert JavaScript Object to JSON string
+const jsonData = JSON.stringify(formDataObject);
+
+      const response = await fetch('https://testnet.gateway.netsepio.com/api/v1.0/profile', {
+        method: 'PATCH',
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth}`,
+        },
+        body: jsonData,
+      });
+
+      if (response.status === 200) {
+        setFormData(initialFormData);
+        setMsg('success');
+        localStorage.setItem('submissionProfile', 'true');
+      } else {
+        setMsg('error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setMsg('error');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -47,7 +144,8 @@ const Profile = () => {
       <section className="pt-24 mb-10">
         <div className="px-5 mx-auto max-w-7xl">
           <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-            <section className="pb-10 rounded-xl" style={bg}>
+           
+            { !profileset && (<section className="pb-10 rounded-xl" style={bg}>
               <div className="px-5 mx-auto max-w-2xl rounded-xl">
                 <div className="w-full mx-auto text-left py-20">
                   <h1 className="mb-8 text-4xl font-bold leading-none tracking-normal text-gray-100 md:text-3xl md:tracking-tight">
@@ -57,7 +155,7 @@ const Profile = () => {
                   <form
                     id="myForm"
                     className="rounded pt-10"
-                   
+                    onSubmit={handleSubmit}
                   >
                     <div className="lg:flex md:flex justify-between">
 
@@ -72,12 +170,12 @@ const Profile = () => {
                       <div className="mb-10 lg:w-2/3 md:w-2/3">
                         <input
                           type="text"
-                          id="title"
+                          id="name"
                           style={border}
                           className="shadow border appearance-none rounded w-full py-4 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
                           placeholder="Your Name"
-                          // value={title}
-                          // onChange={(e) => setTitle(e.target.value)}
+                          value={formData.name}
+              onChange={handleInputChange}
                           required
                         />
                       </div>
@@ -107,13 +205,13 @@ const Profile = () => {
                     <div className="lg:flex md:flex justify-between gap-2">
                     <div className="mb-10 lg:w-1/2 md:w-1/2">
                         <input
-                          type="url"
-                          id="websiteUrl"
+                          type="text"
+                          id="discord"
                           style={border}
                           className="shadow border appearance-none rounded w-full py-4 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
                           placeholder="Discord"
-                          // value={websiteUrl}
-                          // onChange={(e) => setWebsiteUrl(e.target.value)}
+                          value={formData.discord}
+              onChange={handleInputChange}
                           required
                         />
 
@@ -122,13 +220,13 @@ const Profile = () => {
 
                       <div className="mb-10 lg:w-1/2 md:w-1/2">
                         <input
-                          type="url"
-                          id="websiteUrl"
+                          type="text"
+                          id="twitter"
                           style={border}
                           className="shadow border appearance-none rounded w-full py-4 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
                           placeholder="Twitter"
-                          // value={websiteUrl}
-                          // onChange={(e) => setWebsiteUrl(e.target.value)}
+                          value={formData.twitter}
+              onChange={handleInputChange}
                           required
                         />
 
@@ -138,33 +236,18 @@ const Profile = () => {
 
                       <div className="mb-10 lg:w-1/2 md:w-1/2">
                         <input
-                          type="url"
-                          id="websiteUrl"
+                          type="text"
+                          id="country"
                           style={border}
                           className="shadow border appearance-none rounded w-full py-4 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
                           placeholder="Country"
-                          // value={websiteUrl}
-                          // onChange={(e) => setWebsiteUrl(e.target.value)}
+                          value={formData.country}
+              onChange={handleInputChange}
                           required
                         />
 
                   
                       </div>
-
-                    
-
-                    {/* <div className="mb-10">
-                      <textarea
-                        style={border}
-                        id="message"
-                        rows={4}
-                        className="block p-2.5 w-full text-sm text-white bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write your thoughts here..."
-                        // value={description}
-                        // onChange={(e) => setDescription(e.target.value)}
-                        required
-                      ></textarea>
-                    </div> */}
 
                     <div className="text-center pt-10">
                       <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
@@ -187,20 +270,21 @@ const Profile = () => {
             </div>
           </div>
         </div>)}
-{/* {
-   msg == "success" && (
-    <p className="text-green-500">Thankyou! Your review has been submitted successfully.</p>
-   )
-}
+            {
+              msg == "success" && (
+                <p className="text-green-500">Your profile details has been updated successfully.</p>
+              )
+            }
 
-{
-   msg == "error" && (
-    <p className="text-red-500">There is some issue in submitting your review. Try again after sometime.</p>
-   )
-} */}
+            {
+              msg == "error" && (
+                <p className="text-red-500">There is some issue in updating your profile. Try again after sometime.</p>
+              )
+            }
                 </div>
               </div>
             </section>
+            )}
 
 
 
@@ -213,8 +297,10 @@ const Profile = () => {
 
 
 
-
-            <h1 className="mb-8 text-start text-4xl font-bold leading-none tracking-normal text-gray-100 md:text-3xl md:tracking-tight">
+{
+  profileset && (
+    <>
+    <h1 className="mb-8 text-start text-4xl font-bold leading-none tracking-normal text-gray-100 md:text-3xl md:tracking-tight">
                     <span className="text-white">Basic Information</span>
                   </h1>
             <section className="pb-10 rounded-xl" style={bg}>
@@ -319,6 +405,10 @@ const Profile = () => {
                 </div>
               </div>
             </section>
+    </>
+  )
+}
+            
           </div>
         </div>
       </section>
