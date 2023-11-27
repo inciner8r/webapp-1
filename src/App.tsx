164cd51ce8b9e5695412ca7195e6604c9ed0de09
Devsi <grace.emily876@gmail.com>
2023-11-27 13:29:44 +0530

    metadata

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Footer from './Components/Footer';
 import DynamicPage from './pages/DynamicPage';
 import Profile from './pages/Profile';
 import Dashboard from './pages/Dashboard';
+import { Helmet } from 'react-helmet-async';
 
 const App: React.FC = () => {
 
@@ -16,6 +17,57 @@ const App: React.FC = () => {
   }
   
   return (
+    <div>
+      <Helmet>
+        <title>NetSepio</title>
+        <meta
+          name="Description"
+          content="Discover Verified Reviews
+          Empower Your Online Security.
+          Your go-to for reinforcing online security."
+        />
+        <meta property="og:url" content="https://app.netsepio.com/" />
+        <meta
+          name="keywords"
+          content="reviews, security, aptos, domain, verified domain, verified reviews, netsepio"
+        />
+        <meta name="author" content="NetSepio" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta property="og:image" content="/netsepio_logo_light.png" />
+        <meta property="og:image:alt" content="NetSepio" />
+        <meta property="og:image:type" content="image/svg+xml" />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="500" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="NetSepio" />
+        <meta
+          property="og:description"
+          content="Discover Verified Reviews
+          Empower Your Online Security.
+          Your go-to for reinforcing online security."
+        />
+        <meta property="og:image" content="/netsepio_logo_light.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://app.netsepio.com/" />
+        <meta property="twitter:title" content="NetSepio" />
+        <meta
+          property="twitter:description"
+          content="Discover Verified Reviews
+          Empower Your Online Security.
+          Your go-to for reinforcing online security."
+        />
+        <meta property="twitter:image" content="/netsepio_logo_light.png" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/netsepio_logo_light.png" />
+        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/netsepio_logo_light.png" />
+        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/netsepio_logo_light.png" />
+      </Helmet>
     <Router>
       <div style={background}>
         <Navbar/>
@@ -30,6 +82,7 @@ const App: React.FC = () => {
         <Footer />
       </div>
     </Router>
+    </div>
   );
 };
 
