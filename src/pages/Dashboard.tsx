7438fbb13ce70ca089cd10407127ff9fe7823e2a
Devsi <grace.emily876@gmail.com>
2023-11-27 12:05:16 +0530

    changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -348,7 +348,10 @@ console.log("jsonData",jsonData);
                       height="200"
                     />
                     </>
-                  ) :(<label
+                  ) :(
+                  <>
+                  <div className="text-gray-400 -mt-10 pb-4 text-center">Project logo</div>
+                  <label
                         htmlFor="upload"
                         className="flex flex-col items-center gap-2 cursor-pointer mt-20"
                       >
@@ -369,7 +372,9 @@ console.log("jsonData",jsonData);
                             d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                           />
                         </svg>
-                      </label>)}
+                      </label>
+                  </>
+                      )}
                     </div>
                   </div>
                   
