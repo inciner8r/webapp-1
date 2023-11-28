33ef992a3ad8c3f75a6da9d0a812f00b878d1a96
Devsi <grace.emily876@gmail.com>
2023-11-28 20:59:55 +0530

    UI changes

--- src/Components/Buttonnavigation.tsx
+++ src/Components/Buttonnavigation.tsx
@@ -1,50 +1,3 @@
-// // ButtonNavigation.tsx
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// interface ButtonNavigationProps {
-//   onNavigate: (page: string) => void;
-// }
-
-// const ButtonNavigation: React.FC<ButtonNavigationProps> = ({ onNavigate }) => {
-//   const [activeButton, setActiveButton] = useState<string>('reviews');
-
-//   const changePage = (page: string) => {
-//     setActiveButton(page);
-//     onNavigate(page);
-//   };
-
-//   return (
-//     <div className="inline-flex items-center justify-center w-full gap-4 mb-10">
-//       <Link
-//       to="/view-my-reviews"
-//         onClick={() => changePage('reviews')}
-//         className={`border rounded-lg px-10 py-8 ${activeButton === 'reviews' ? 'bg-white text-black' : 'text-white'}`}
-//       >
-//         Reviews
-//       </Link>
-//       <Link
-//       to="/dashboard"
-//         onClick={() => changePage('projects')}
-//         className={`border rounded-lg px-10 py-8 ${activeButton === 'projects' ? 'bg-white text-black' : 'text-white'}`}
-//       >
-//         Projects
-//       </Link>
-//       <Link
-//       to="/vpn"
-//         onClick={() => changePage('vpns')}
-//         className={`border rounded-lg px-12 py-8 ${activeButton === 'vpns' ? 'bg-white text-black' : 'text-white'}`}
-//       >
-//         VPNs
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default ButtonNavigation;
-
-
-
 // ButtonNavigation.tsx
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -69,27 +22,36 @@ const ButtonNavigation: React.FC<ButtonNavigationProps> = ({ onNavigate }) => {
       <Link
         to="/view-my-reviews"
         onClick={() => changePage('reviews')}
-        className={`text-white border rounded-lg px-10 py-8 ${
-          location.pathname.includes('view-my-reviews') ? 'bg-white text-black' : ''
-        }`}
+        className={`rounded-lg px-10 py-8 font-bold`}
+        style={{
+            color: location.pathname.includes('view-my-reviews') ? 'white' : '#788AA3',
+            backgroundColor: location.pathname.includes('view-my-reviews') ? '#4B5995' : '',
+            borderBottom: location.pathname.includes('view-my-reviews') ? '2px solid white' : '',
+          }}
       >
         Reviews
       </Link>
       <Link
         to="/dashboard"
         onClick={() => changePage('projects')}
-        className={`text-white border rounded-lg px-10 py-8 ${
-          location.pathname.includes('dashboard') ? 'bg-white text-black' : ''
-        }`}
+        className={`rounded-lg px-10 py-8 font-bold`}
+        style={{
+            color: location.pathname.includes('dashboard') ? 'white' : '#788AA3',
+            backgroundColor: location.pathname.includes('dashboard') ? '#4B5995' : '',
+            borderBottom: location.pathname.includes('dashboard') ? '2px solid white' : '',
+          }}
       >
         Projects
       </Link>
       <Link
         to="/vpn"
         onClick={() => changePage('vpns')}
-        className={`text-white border rounded-lg px-12 py-8 ${
-          location.pathname.includes('vpn') ? 'bg-white text-black' : ''
-        }`}
+        className={`rounded-lg px-12 py-8 font-bold`}
+        style={{
+            color: location.pathname.includes('vpn') ? 'white' : '#788AA3',
+            backgroundColor: location.pathname.includes('vpn') ? '#4B5995' : '',
+            borderBottom: location.pathname.includes('vpn') ? '2px solid white' : '',
+          }}
       >
         VPNs
       </Link>
